39032d59e468a6265e4afe7551950b2fc8723627
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-10-15 22:47:06 -0700

    Fixed some errors showing up when no images are present.

--- index.php
+++ index.php
@@ -39,11 +39,13 @@
     <?php endif; ?>
     
     <div id="galleryListWrapper">
-        <ul id="galleryList" class="clearfix">
-            <?php foreach ($galleryArray['images'] as $image): ?>
-                <li><a href="<?php echo $image['file_path']; ?>" title="<?php echo $image['file_title']; ?>" rel="colorbox"><img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>"/></a></li>
-            <?php endforeach; ?>
-        </ul>
+        <?php if (!empty($galleryArray) && $galleryArray['stats']['total_images'] > 0): ?>
+            <ul id="galleryList" class="clearfix">
+                <?php foreach ($galleryArray['images'] as $image): ?>
+                    <li><a href="<?php echo $image['file_path']; ?>" title="<?php echo $image['file_title']; ?>" rel="colorbox"><img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>"/></a></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
     </div>
     
     <div class="line"></div>
