834941743e2013530993ce209f2e2421605606cc
xy1 <mattscandale@hotmail.com>
2014-02-23 19:14:39 -0600

    Update index.php
    Added "html_entity_decode" function to Line 39 to allow launching a file named something like "new&old.png", which would otherwise try to (unsuccessfully) launch as "new&amp;old.png".

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         <?php if (!empty($galleryArray) && $galleryArray['stats']['total_images'] > 0): ?>
             <ul id="galleryList" class="clearfix">
                 <?php foreach ($galleryArray['images'] as $image): ?>
-                    <li><a href="<?php echo $image['file_path']; ?>" title="<?php echo $image['file_title']; ?>" rel="colorbox"><img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>"/></a></li>
+                    <li><a href="<?php echo html_entity_decode($image['file_path']); ?>" title="<?php echo $image['file_title']; ?>" rel="colorbox"><img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>"/></a></li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
